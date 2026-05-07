7dc62aab3898a5b411d53234e07c8461eb616823
Sayyed Aaman <sayyedaaman9@gmail.com>
2026-05-07 21:02:38 +0530

    Attached routes to app.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import corsMiddleware from './middleware/cors.middleware.js'
 import globalErrorHandler from './middleware/error.middleware.js';
 import loggerMiddleware from './middleware/logger.middleware.js'
+
+import RootRoutes from './routes/index.js';
 const app = express();
 
 app.use(corsMiddleware);
@@ -11,7 +13,7 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-
+app.use("/api/v1", RootRoutes);
 
 app.use(globalErrorHandler);
 
