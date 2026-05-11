91443e5f5be1db37f2e85ba5cdb1e3659f987e25
Sayyed Aaman <sayyedaaman9@gmail.com>
2026-05-11 21:11:56 +0530

    health check endpoint

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import corsMiddleware from './middleware/cors.middleware.js'
 import globalErrorHandler from './middleware/error.middleware.js';
+import helmetMiddleware from './middleware/helmet.middleware.js';
 import loggerMiddleware from './middleware/logger.middleware.js'
 import webhookRoutes from './routes/webhook.routes.js';
 import RootRoutes from './routes/index.js';
@@ -21,6 +22,14 @@ app.use(loggerMiddleware);
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
+app.get("/health",(req,res)=>{
+  res.status(200).json({
+    success:true,
+    uptime: process.uptime(),
+    timestamp: new Date(),
+    environment: process.env.NODE_ENV
+  })
+})
 
 app.use("/api/v1/webhook", webhookRoutes);
 app.use("/api/v1", RootRoutes);
