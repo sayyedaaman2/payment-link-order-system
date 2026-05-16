77264d3b364e6702375b07a6de72fb50650b4ee8
Sayyed Aaman <sayyedaaman9@gmail.com>
2026-05-16 22:29:55 +0530

    impelemet graceful server shutdown handling

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ import env from "./config/env.js";
 import app from './app.js';
 import {connectDB} from './lib/database.js';
 import logger from './util/logger.js';
-async function server(){
+import mongoose from "mongoose";
+
+let server;
+async function serverStart(){
 try{
     await connectDB();
-    app.listen(env.PORT, () => {
+    server = app.listen(env.PORT, () => {
         logger.info(`Server running in ${process.env.NODE_ENV} mode on port ${env.PORT}`);
     });
 }catch(err){
@@ -14,4 +17,33 @@ try{
 }
 }
 
-server();
+serverStart();
+
+
+//  Graceful Shutdown Handler
+
+const gracefulShutDown = async (signal)=>{
+    logger.info(
+        `${signal} received. Shutting down server gracefully...`
+    );
+
+    server.close(async()=>{
+        logger.info("HTTP server closed")
+    })
+
+    try{
+        await mongoose.connection.close();
+
+        logger.info("MongoDB connection closed")
+        process.exit(0);
+    }catch(error){
+        logger.error(err, "Error while closing MongoDB");
+        process.exit(1);
+    }
+}
+
+// Process Signals
+
+process.on("SIGINT",()=> gracefulShutDown("SIGINT"));
+
+process.on("SIGTERM",()=> gracefulShutDown("SIGTERM"))
